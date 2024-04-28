c2caabd2b72d084ec5fbb25dc878e0251cb29836
fjvalverde <fjvalverde@iccaweb.com>
2024-04-28 23:14:31 +0200

    Entity framework

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pagination_example_back.Models;
+
+namespace pagination_example_back.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace pagination_example_back.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
